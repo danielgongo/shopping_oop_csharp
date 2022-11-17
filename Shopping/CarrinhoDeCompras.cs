47503a92b062ab6dce1ce5111cac8b701b19859d
Daniel Gongo <dan_liveira@outlook.com>
2022-11-17 10:34:01 -0300

    Projeto carrinho de compras com c#

---
+++ Shopping/CarrinhoDeCompras.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    public class CarrinhoDeCompras : Produto
+    {
+        List<Produto> carrinho = new List<Produto>();
+
+        public CarrinhoDeCompras(int codigo, string nome, double preco) : base(codigo, nome, preco)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public CarrinhoDeCompras()
+        {
+
+        }
+
+   
+
+        public void AddProdutos(Produto produto)
+        {
+            carrinho.Add(produto);
+        }
+
+        public void RemoveProdutos(Produto produto)
+        {
+            carrinho.Remove(produto);
+        }
+
+        public double TotalPreco()
+        {
+            double somaPreco = 0.0;
+
+            foreach(Produto produto in carrinho)
+            {
+                somaPreco += produto.Preco;
+            }
+            return somaPreco;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Produto produto in carrinho)
+            {
+                sb.Append(produto + "\n");
+            }
+
+            return sb.ToString();
+        }
+        
+    }
+}
---
+++ Shopping/Principal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Produto p1 = new Produto(11, "Mouse", 22.0);
+            Produto p2 = new Produto(22, "Teclado", 33.0);
+            Produto p3 = new Produto(33, "Monitor", 77.0);
+            Produto p4 = new Produto(44, "CPU", 99.0);
+
+            Console.Write("Código: ");
+            int codigo = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Produto p5 = new Produto(codigo, nome, preco);
+
+            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
+
+
+
+
+            carrinho.AddProdutos(p1);
+            carrinho.AddProdutos(p2);
+            carrinho.AddProdutos(p3);
+            carrinho.AddProdutos(p4);
+            Console.WriteLine();
+            carrinho.AddProdutos(p5);
+
+            carrinho.RemoveProdutos(p4);
+
+
+
+            Console.WriteLine(carrinho);
+            Console.WriteLine("Total R$: " + carrinho.TotalPreco());
+
+
+            Console.ReadKey();  
+        }
+    }
+}
